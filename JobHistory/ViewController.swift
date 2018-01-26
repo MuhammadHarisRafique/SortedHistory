712c16669da8e7d714c03e776feb31b21c368df0
haris <haris.math@gmail.com>
2018-01-26 17:26:58 +0500

    apply swipe gesture

--- JobHistory/ViewController.swift
+++ JobHistory/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UIGestureRecognizerDelegate {
 
     @IBOutlet weak var progress: UIButton!
     @IBOutlet weak var navBarView: UIView!
     @IBOutlet weak var tableView: UITableView!
+    
+    
     var booked = ["booked","booked","booked","booked","booked","booked","booked","booked"]
     var cancel = ["cancelled","cancelled","cancelled","cancelled","cancelled","cancelled","cancelled"]
     var progressJob = ["progress","progress","progress","progress","progress","progress","progress","progress"]
@@ -26,11 +28,50 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         
         self.array = self.booked
         self.makeunderline(button: self.booking_Pressed)
+        self.tableView.tableFooterView = UIView()
+
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(self.abc(sender:)))
+        swipe.numberOfTouchesRequired = 1
+        swipe.delegate = self
+        self.tableView.isUserInteractionEnabled = true
+        self.tableView.addGestureRecognizer(swipe)
         
-        
-       
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        print("touch")
+        
+    }
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false
+    }
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+
+        return true
+        
+    }
+    @objc func abc(sender:UISwipeGestureRecognizer){
+        //let direction = sender.direction
+        print(sender.direction.rawValue)
+        switch sender.direction {
+            
+        case .left:
+            
+            print("left")
+            
+        case .right:
+            
+            print("right")
+            
+        default:
+            
+            print("default")
+        
+        }
+    
+       
+    }
     private func makeunderline(button:UIButton){
         
         self.underline.removeFromSuperview()
@@ -38,8 +79,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         self.underline.frame = frame
         self.underline.layer.backgroundColor = UIColor.white.cgColor
         self.navBarView.addSubview(self.underline)
-        
-        
+    
     }
     
     @IBOutlet weak var booking_Pressed: UIButton!
@@ -47,24 +87,30 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     
     @IBAction func booking_Pressed(_ sender: UIButton) {
+        
         self.array = []
         self.array = self.booked
         self.tableView.reloadData()
         self.makeunderline(button: self.booking_Pressed)
+        
     }
     
     @IBAction func cancelled_pressed(_ sender: UIButton) {
+        
         self.array = []
         self.array = self.cancel
         self.tableView.reloadData()
-         self.makeunderline(button: self.cancelled)
+        self.makeunderline(button: self.cancelled)
+        
     }
     
     @IBAction func progress_Pressed(_ sender: UIButton) {
+        
         self.array = []
         self.array = self.progressJob
         self.tableView.reloadData()
-         self.makeunderline(button: self.progress)
+        self.makeunderline(button: self.progress)
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
